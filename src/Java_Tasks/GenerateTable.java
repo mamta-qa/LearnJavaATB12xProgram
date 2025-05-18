572d696005ed36e35756082746f47c4777c2e9f4
mamta-qa <mamtadeeju2@gmail.com>
2025-05-18 10:36:37 +0400

    Created Class for Table Generation task

--- src/Java_Tasks/GenerateTable.java
+++ src/Java_Tasks/GenerateTable.java
@@ -3,6 +3,33 @@ package Java_Tasks;
 public class GenerateTable {
 
     public static void main(String[] args) {
+        int num=5;
+        System.out.printf("%d*1=%d",num, num*1);
+        System.out.println();
+        System.out.printf("%d*2=%d",num, num*2);
+        System.out.println();
+        System.out.printf("%d*3=%d",num, num*3);
+        System.out.println();
+        System.out.printf("%d*4=%d",num, num*4);
+        System.out.println();
+        System.out.printf("%d*5=%d",num, num*5);
+        System.out.println();
+        System.out.printf("%d*6=%d",num, num*6);
+        System.out.println();
+        System.out.printf("%d*7=%d",num, num*7);
+        System.out.println();
+        System.out.printf("%d*8=%d",num, num*8);
+        System.out.println();
+        System.out.printf("%d*9=%d",num, num*9);
+        System.out.println();
+        System.out.printf("%d*10=%d",num, num*10);
+        System.out.println();
+
+
+
+
+
+
 
     }
 }
