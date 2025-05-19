7fba7acc8483cfb37799a3f75fef2a5d5af2ad31
mamta-qa <mamtadeeju2@gmail.com>
2025-05-19 08:49:43 +0400

    Ternarity operators

---
+++ src/Java_Tasks/operator_increment2.java
@@ -0,0 +1,16 @@
+package Java_Tasks;
+
+public class operator_increment2 {
+    public static void main(String[] args) {
+
+        int a = 11, b = 12, c;
+
+        c = a + b + a++ + b++ + ++a + ++b;
+        System.out.println("a=" +a);
+        System.out.println("b=" +b);
+        System.out.println("c=" +c);
+
+
+
+    }
+}
---
+++ src/Java_Tasks/operators_Increment.java
@@ -0,0 +1,11 @@
+package Java_Tasks;
+
+public class operators_Increment {
+    public static void main(String[] args) {
+        int i=10;
+
+        i= i++ + ++i;
+        System.out.println(i);
+
+    }
+}
