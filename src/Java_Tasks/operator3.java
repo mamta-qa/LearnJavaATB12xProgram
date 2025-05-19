27c10fa75d9ea4940a1cb93ea329d4a8074a7ae2
mamta-qa <mamtadeeju2@gmail.com>
2025-05-19 09:36:01 +0400

    Ternarity operators

---
+++ src/Java_Tasks/operator3.java
@@ -0,0 +1,11 @@
+package Java_Tasks;
+
+public class operator3 {
+    public static void main(String[] args) {
+        int i=1, j=2,k=3;
+
+        int m=i-- - j-- - j - k--;
+        System.out.println("i=" + i + ", j=" + j + ", k=" + k + ", m=" + m);
+
+    }
+}
--- src/Java_Tasks/operator_increment2.java
+++ src/Java_Tasks/operator_increment2.java
@@ -3,7 +3,7 @@ package Java_Tasks;
 public class operator_increment2 {
     public static void main(String[] args) {
 
-        int a = 11, b = 12, c;
+        int a = 11, b = 22, c;
 
         c = a + b + a++ + b++ + ++a + ++b;
         System.out.println("a=" +a);
