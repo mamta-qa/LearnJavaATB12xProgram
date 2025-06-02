a50431c08a225a3460b7efc21b16e4dfcc6bb91c
mamta-qa <mamtadeeju2@gmail.com>
2025-06-02 15:57:12 +0400

    Ternarity operators

---
+++ src/DoWhileLoops/whileloop.java
@@ -0,0 +1,17 @@
+package DoWhileLoops;
+
+public class whileloop {
+        public static void main(String[] args) {
+
+            int i= 0;
+
+            while ( i<10) {
+                System.out.println(i);
+                i++;
+            }
+            for ( int j=0; j<10; j++)
+                System.out.println("value of" +j);
+        }
+    }
+
+
