a63dbd4547f54b049f554f23d60191009dc4f0d7
mamta-qa <mamtadeeju2@gmail.com>
2025-06-21 10:22:21 +0400

    CodingChallenge2

---
+++ src/CodingTest/CodingChallenge2.java
@@ -0,0 +1,20 @@
+package CodingTest;
+
+public class CodingChallenge2 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        int sum = a + b;
+        int sub = b-a;
+        int multi= a*b;
+        int mod = a/b;
+        int div= a%b;
+        boolean equal = a==b;
+
+        {
+            System.out.println(" Addition : " + sum+ " Substraction :" +sub+  " Multiplication :" +multi+ " Division: " +div+ " Modulus:"  +mod+ "  Equal : " +equal);
+        }
+    }}
+
