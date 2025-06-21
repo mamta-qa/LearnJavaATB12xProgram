de9eb46505ba6c7c34c605de229033aa0cbc007d
mamta-qa <mamtadeeju2@gmail.com>
2025-06-21 18:14:26 +0400

    CodingChallenge4

---
+++ src/CodingTest/CodingChallenge4.java
@@ -0,0 +1,27 @@
+package CodingTest;
+
+public class CodingChallenge4 {
+    public static void main(String[] args) {
+
+        int i=5;
+
+        System.out.println(" initial value of i :" +i);
+           System.out.println(" post increment :"  + (i++));
+        System.out.println(" after increment :"  +(i));
+
+
+        System.out.println(" pre increment :"  +(++i));
+        System.out.println(" after increment :"  +(i));
+
+        System.out.println(" pre decrement :"  +(--i));
+        System.out.println(" after deccrement :"  +(i));
+
+        System.out.println(" after decrement :"  + (i--));
+        System.out.println(" after deccrement :"  +(i));
+
+
+
+
+
+
+    }}
