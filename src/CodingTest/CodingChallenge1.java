53b8cdfc0c385afbed7c55d00260e6f38f374ba9
mamta-qa <mamtadeeju2@gmail.com>
2025-06-21 10:10:58 +0400

    CodingChallenge1

---
+++ src/CodingTest/CodingChallenge1.java
@@ -0,0 +1,15 @@
+package CodingTest;
+
+public class CodingChallenge1 {
+    public static void main(String[] args) {
+
+        int i = 25;
+
+        double d = 3.14;
+        boolean b = true;
+        char a = 'A';
+        System.out.println("Integer: " + i + "Double: " + d + " Boolean: " + b + "  character: " + a);
+
+
+    }
+}
