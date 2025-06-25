48a81a52646eb6a36d89f2481f95e308321d3a85
mamta-qa <mamtadeeju2@gmail.com>
2025-06-25 21:37:12 +0400

    constructors

---
+++ src/Constructors/DeafultConstructor.java
@@ -0,0 +1,13 @@
+package Constructors;
+
+public class DeafultConstructor {
+
+    DeafultConstructor(){
+        System.out.println("this is default constructor");
+    }
+
+    public static void main(String[] args) {
+        DeafultConstructor obj = new DeafultConstructor();
+        
+    }
+}
